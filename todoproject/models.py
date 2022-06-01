1e4de076e28c52c0c85574f2b84efef687c5561b
Stefan Cosic <stefan22bl@gmail.com>
2022-06-01 06:11:56 +0200

    addedModelsAndConfigDbAndAuth

---
+++ todoproject/models.py
@@ -0,0 +1,50 @@
+from email.policy import default
+from enum import unique
+from todoproject import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+
+class User(db.Model, UserMixin):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    todos = db.relationship('Todos', backref='author', lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f'Username: {self.username}'
+
+
+class Todos(db.Model, UserMixin):
+
+    __tablename__ = 'todos'
+
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    text = db.Column(db.Text, nullable=False)
+
+    def __init__(self, text, user_id):
+        self.text = text
+        self.user_id = user_id
+    
+    def __repr__(self):
+        return f'Todo ID: {self.id} -- Date: {self.date} -- Text: {self.text}'
+
